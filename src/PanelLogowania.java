07f0ca331c2d60c9d1f88f9558436b2eaa92055a
Amarylis <amaryliska@gmail.com>
2016-04-01 18:27:16 +0200

    Panel główny - panel logowania

--- src/PanelLogowania.java
+++ src/PanelLogowania.java
@@ -27,6 +27,14 @@ public class PanelLogowania extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanelPanelLogowania = new javax.swing.JPanel();
+        jTextFieldLoginPanelLogowania = new javax.swing.JTextField();
+        jTextFieldHasloPanelLogowania = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jButtonWyjsciePanelLogowania = new javax.swing.JButton();
+        jButtonZalogujPanelLogowania = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jLabelRejestracjaPanelLogowania = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
@@ -34,15 +42,74 @@ public class PanelLogowania extends javax.swing.JFrame {
         setResizable(false);
         setSize(new java.awt.Dimension(600, 400));
 
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel1.setText("Login:");
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel2.setText("Hasło:");
+
+        jButtonWyjsciePanelLogowania.setText("Wyjście");
+
+        jButtonZalogujPanelLogowania.setText("Zaloguj");
+        jButtonZalogujPanelLogowania.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonZalogujPanelLogowaniaActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("Nie masz konta?");
+
+        jLabelRejestracjaPanelLogowania.setForeground(new java.awt.Color(51, 51, 255));
+        jLabelRejestracjaPanelLogowania.setText("Zarejestruj się!");
+
         javax.swing.GroupLayout jPanelPanelLogowaniaLayout = new javax.swing.GroupLayout(jPanelPanelLogowania);
         jPanelPanelLogowania.setLayout(jPanelPanelLogowaniaLayout);
         jPanelPanelLogowaniaLayout.setHorizontalGroup(
             jPanelPanelLogowaniaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 600, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelPanelLogowaniaLayout.createSequentialGroup()
+                .addGroup(jPanelPanelLogowaniaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanelPanelLogowaniaLayout.createSequentialGroup()
+                        .addGap(133, 133, 133)
+                        .addComponent(jButtonWyjsciePanelLogowania, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 97, Short.MAX_VALUE)
+                        .addComponent(jButtonZalogujPanelLogowania, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(34, 34, 34))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelPanelLogowaniaLayout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(jPanelPanelLogowaniaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(40, 40, 40)
+                        .addGroup(jPanelPanelLogowaniaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanelPanelLogowaniaLayout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jLabelRejestracjaPanelLogowania, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanelPanelLogowaniaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(jTextFieldLoginPanelLogowania, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jTextFieldHasloPanelLogowania, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                .addGap(114, 114, 114))
         );
         jPanelPanelLogowaniaLayout.setVerticalGroup(
             jPanelPanelLogowaniaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(jPanelPanelLogowaniaLayout.createSequentialGroup()
+                .addGap(121, 121, 121)
+                .addGroup(jPanelPanelLogowaniaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextFieldLoginPanelLogowania, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addGap(26, 26, 26)
+                .addGroup(jPanelPanelLogowaniaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextFieldHasloPanelLogowania, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addGap(31, 31, 31)
+                .addGroup(jPanelPanelLogowaniaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabelRejestracjaPanelLogowania))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 54, Short.MAX_VALUE)
+                .addGroup(jPanelPanelLogowaniaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButtonZalogujPanelLogowania)
+                    .addComponent(jButtonWyjsciePanelLogowania))
+                .addGap(83, 83, 83))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -58,6 +125,10 @@ public class PanelLogowania extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButtonZalogujPanelLogowaniaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonZalogujPanelLogowaniaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButtonZalogujPanelLogowaniaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -95,6 +166,14 @@ public class PanelLogowania extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButtonWyjsciePanelLogowania;
+    private javax.swing.JButton jButtonZalogujPanelLogowania;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabelRejestracjaPanelLogowania;
     private javax.swing.JPanel jPanelPanelLogowania;
+    private javax.swing.JTextField jTextFieldHasloPanelLogowania;
+    private javax.swing.JTextField jTextFieldLoginPanelLogowania;
     // End of variables declaration//GEN-END:variables
 }
