f2c88a23301f64c5e45bd2f322f449d0f6cd28e8
Amaryliska <amaryliska@gmail.com>
2016-05-07 13:48:43 +0200

    [Panel Użytkownika] Rezerwacja biletów

--- BiletyLotniczeProjekt/src/Formatki/AktualneLoty.java
+++ BiletyLotniczeProjekt/src/Formatki/AktualneLoty.java
@@ -49,7 +49,7 @@ public class AktualneLoty extends javax.swing.JFrame {
         jScrollPane2 = new javax.swing.JScrollPane();
         listaAktualnychLotow = new javax.swing.JTable();
         jLabel3 = new javax.swing.JLabel();
-        wykonajPrzycisk = new javax.swing.JButton();
+        wyszukajPrzycisk = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
@@ -97,13 +97,21 @@ public class AktualneLoty extends javax.swing.JFrame {
         });
         listaAktualnychLotow.setRowHeight(30);
         jScrollPane2.setViewportView(listaAktualnychLotow);
+        if (listaAktualnychLotow.getColumnModel().getColumnCount() > 0) {
+            listaAktualnychLotow.getColumnModel().getColumn(0).setPreferredWidth(30);
+            listaAktualnychLotow.getColumnModel().getColumn(2).setPreferredWidth(20);
+            listaAktualnychLotow.getColumnModel().getColumn(3).setPreferredWidth(50);
+            listaAktualnychLotow.getColumnModel().getColumn(4).setPreferredWidth(30);
+            listaAktualnychLotow.getColumnModel().getColumn(5).setPreferredWidth(10);
+            listaAktualnychLotow.getColumnModel().getColumn(6).setPreferredWidth(10);
+        }
 
         jLabel3.setText("Data lotu:");
 
-        wykonajPrzycisk.setLabel("Wykonaj");
-        wykonajPrzycisk.addActionListener(new java.awt.event.ActionListener() {
+        wyszukajPrzycisk.setText("Szukaj");
+        wyszukajPrzycisk.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                wykonajPrzyciskActionPerformed(evt);
+                wyszukajPrzyciskActionPerformed(evt);
             }
         });
 
@@ -129,22 +137,26 @@ public class AktualneLoty extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(24, 24, 24)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 436, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 637, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(wykonajPrzycisk))
-                    .addComponent(jLabel1)
+                        .addGap(24, 24, 24)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 436, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel1)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jCheckBox1)
+                                .addGap(10, 10, 10)
+                                .addComponent(jCheckBox2)
+                                .addGap(18, 18, 18)
+                                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 184, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(160, 160, 160)
+                                .addComponent(wyszukajPrzycisk)))
+                        .addGap(0, 321, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jCheckBox1)
-                        .addGap(10, 10, 10)
-                        .addComponent(jCheckBox2)
-                        .addGap(18, 18, 18)
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 184, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(146, Short.MAX_VALUE))
+                        .addContainerGap()
+                        .addComponent(jScrollPane2)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -153,27 +165,27 @@ public class AktualneLoty extends javax.swing.JFrame {
                 .addComponent(jLabel2)
                 .addGap(17, 17, 17)
                 .addComponent(jLabel3)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jCheckBox1)
-                    .addComponent(jCheckBox2)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jCheckBox1)
+                            .addComponent(jCheckBox2)
+                            .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(wyszukajPrzycisk)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 328, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(79, 79, 79))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(wykonajPrzycisk)
-                        .addGap(104, 104, 104))))
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 328, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(79, 79, 79))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void wykonajPrzyciskActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_wykonajPrzyciskActionPerformed
+    private void wyszukajPrzyciskActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_wyszukajPrzyciskActionPerformed
         // TODO add your handling code here:
         String przylotyOdloty = null;
         String klasa = null;
@@ -205,7 +217,7 @@ public class AktualneLoty extends javax.swing.JFrame {
         {
             Logger.getLogger(AktualneLoty.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }//GEN-LAST:event_wykonajPrzyciskActionPerformed
+    }//GEN-LAST:event_wyszukajPrzyciskActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -225,6 +237,6 @@ public class AktualneLoty extends javax.swing.JFrame {
     private javax.swing.JRadioButtonMenuItem jRadioButtonMenuItem1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable listaAktualnychLotow;
-    private javax.swing.JButton wykonajPrzycisk;
+    private javax.swing.JButton wyszukajPrzycisk;
     // End of variables declaration//GEN-END:variables
 }
